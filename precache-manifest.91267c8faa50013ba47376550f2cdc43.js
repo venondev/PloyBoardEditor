559a2f559b26ba810400e78a84d3873e10f0c8e4
lm <hjalmar.schulz@web.de>
2020-02-05 23:16:32 +0100

    Updates

---
+++ precache-manifest.91267c8faa50013ba47376550f2cdc43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea748787c79d60fd472459ae902f6618",
+    "url": "/PloyBoardEditor/index.html"
+  },
+  {
+    "revision": "dc0cd7a5bbba74b2751f",
+    "url": "/PloyBoardEditor/static/css/main.91c0cd4d.chunk.css"
+  },
+  {
+    "revision": "5187f15d4416310a2ddd",
+    "url": "/PloyBoardEditor/static/js/2.b8b0857a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PloyBoardEditor/static/js/2.b8b0857a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0cd7a5bbba74b2751f",
+    "url": "/PloyBoardEditor/static/js/main.a255d26b.chunk.js"
+  },
+  {
+    "revision": "94974ad507ae0f479fc4",
+    "url": "/PloyBoardEditor/static/js/runtime-main.95668189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PloyBoardEditor/static/media/logo.5d5d9eef.svg"
+  }
+]);
